5013316c72fd54e0cbd65abfe409fab7406b29d2
kosatun@aol.com <kosatun@aol.com>
2018-07-05 21:19:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,19 +10,7 @@ function nestedTarget() {
 
 function increaseRankBy(n) {
   var increase = document.querySelectorAll('.ranked-list');
-  const firstList = increase[0];
-  const secondList = increase[1];
-  let children = firstList.children;
-  let start = 1;
-      for (let i = 0, l = children.length; i < l; i++) {
-       return  parseInt(children[i + n]);
+  for (let i = 0; i < n; i++) {
+       return  parseInt(increase[i + n]);
       }
-
-      children = secondList.children;
-      start = 12;
-
-      for (let i = 0, l = children.length; i < l; i++) {
-        return parseInt(children[i + n]);
-
-}
 }
