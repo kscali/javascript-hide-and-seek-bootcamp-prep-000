6802b2803dcc1445d440f592c86537307c516fec
kosatun@aol.com <kosatun@aol.com>
2018-07-05 22:12:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,16 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
- 
+ const rankedLists = document.querySelectorAll('.ranked-list');
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children;
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
+}
 
 function deepestChild() {
   var deepChild = document.querySelector('#grand-node').querySelectorAll('div');
