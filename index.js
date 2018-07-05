4e8c31c3fc569a48f368c26df01627c765891694
kosatun@aol.com <kosatun@aol.com>
2018-07-05 20:47:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function getFirstSelector(selector) {
   var first = document.querySelector(selector);
   return first;
 }
+
+function nestedTarget() {
+  var nested = document.getElementById('nested').querySelector('.target');
+  return nested;
+}
