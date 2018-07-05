dd76f2c79d76f4752fd8ab376481625d1a42e5b6
kosatun@aol.com <kosatun@aol.com>
2018-07-05 22:10:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ function increaseRankBy(n) {
 
   let children = firstList.children
   let start = 1
-  for (let i = 0, l = children.length; i < l; i++) {
+  for (let i = 0, start = children.length; i < l; i++) {
     parseInt(children[i].innerHTML) + start + n;
 
     children = secondList.children
     start = 12
 
-    for (let i = 0, l2 = children.length; i < l; i++) {
+    for (let i = 0, start = children.length; i < l; i++) {
       parseInt(children[i].innerHTML) + start + n
       }
    }
